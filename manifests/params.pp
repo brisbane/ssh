8d8b27f2f3eff711f45590232f273823aad7a34b
kashif74 <kashif.alig@gmail.com>
2015-11-13 12:15:22 +0000

    minor changes

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ssh::params {
   #This should be a hiera variable, so that we get default with overrides  behaviour
-  $sshd_parameters = hiera('ssh::params::sshd_parameters', {})
+  $sshd_parameters = hiera('ssh::params::sshd_parameters', [])
   $ssh_parameters = hiera_hash('ssh::params::ssh_parameters', { })
                                        
   $enable_authorized_keys = hiera('ssh::params::enable_authorized_keys', true)
@@ -10,6 +10,6 @@ class ssh::params {
   # Enable or disable tcp_wrapper
   $enable_tcp_wrapper = hiera ('ssh::params::enable_tcp_wrapper', true)
   $allow  = hiera_hash ('ssh::params::tcp_wrapper_allow', {})
-  $deny  = hiera ('ssh::params::tcp_wrapper_deny', {'pplxint6' => 'GSSAPIAuthentication yes'}) 
+  $deny  = hiera ('ssh::params::tcp_wrapper_deny', {}) 
   
 }
